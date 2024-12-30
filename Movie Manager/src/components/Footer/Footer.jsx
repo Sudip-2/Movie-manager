486fa765eaf8e15a87562270cf56af579c597733
Sudip Paul <p452570@gmail.com>
2024-12-30 18:36:26 +0530

    added footer

--- Movie Manager/src/components/Footer/Footer.jsx
+++ Movie Manager/src/components/Footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Logo from '../Header/Logo'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <footer className="text-gray-300 body-font bg-gray-900">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <div className="flex title-font font-medium items-center md:justify-start justify-center text-gray-300">
+          <Logo />
+        </div>
+        <p className="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
+          © 2024 Sudip Paul —
+          <a href="https://www.linkedin.com/in/sudip-paul18/" className="text-gray-300 ml-1" rel="noopener noreferrer" target="_blank">@Sudip</a>
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <a className="ml-3 text-gray-300" href='https://x.com/gm_sudip' target='_blank'>
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 50 50" >
+              <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-300" href='https://www.linkedin.com/in/sudip-paul18/' target='_blank'>
+            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
+              <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+          <a className="text-gray-300" href='https://www.facebook.com/profile.php?id=100036912233666' target='_blank'>
+            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-300" href='https://www.instagram.com/anime_sup17/' target='_blank'>
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+          </a>
+        </span>
+      </div>
+    </footer>
+
   )
 }
 
