61622f309d1a0f30d6da404aca50b1d5b06ff66f
Sudip Paul <p452570@gmail.com>
2025-01-03 01:33:36 +0530

    added search

--- Movie Manager/src/Search/Search.jsx
+++ Movie Manager/src/Search/Search.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
+import Homediv from '../Home/Homediv';
 
 const Search = () => {
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const value = queryParams.get('title');
+  let [arrayLength,setArraylength] = useState(0)
+ 
+  let [content, setContent] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      let apikey = import.meta.env.VITE_Apikey
+      let apiUrl = `https://www.omdbapi.com/?apikey=${apikey}&s=${value}&plot=full`
+
+      let data = await fetch(apiUrl)
+      let response = await data.json()
+      let dataArray = await response.Search
+      setArraylength(dataArray.length)
+      let titleImg = dataArray.map((item) => {
+        return (
+          <Homediv item={item} key={item.imdbID}/>
+        )
+      })
+      setContent(titleImg)
+    }
+    fetchData()
+
+  }, [value])
+
   return (
     <>
-    <div className='h-screen'>Search</div>
+    <div className=' bg-gray-800 text-gray-300 px-2 min-h-[90svh]'>
+        <div className='max-w-[1200px] mx-auto pt-[60px] pb-[20px] sm:py-[30px] h-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-8'>
+
+          <div className='col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 font-Lato font-bold'>
+            <p>Search Results {arrayLength}</p>
+          </div>
+
+          {content}
+
+          {/* <div className='w-full aspect-[2/3] z-[1] relative overflow-hidden'>
+
+            <img src="https://imgs.search.brave.com/KuW3HcPR2IwH4a4ThKmnN4Y_uGBdhzY2oDHmhfdVft0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuZm9udHNpbnVz/ZS5jb20vc3RhdGlj/L3VzZS1tZWRpYS1p/dGVtcy8xODQvMTgz/MDgyL3VwdG8tNzAw/eGF1dG8vNjNkZjk3/ZjcvMTAyOF9NLTAz/MTRfZm90b19kLmpw/ZWc" alt='posters' className='w-full h-full object-cover rounded-md hover:scale-110 duration-300' />
+
+              <p className='absolute text-red-600 left-2 bottom-2 backdrop-blur-lg border px-2 text-sm rounded-full py-1' >Added</p>          
+
+            <abbr title="Add to Your watchlist">
+
+            </abbr>
+
+          </div> */}
+          
+
+        </div>
+
+      </div>
     </>
   )
 }
--- Movie Manager/src/Watchlist/Watchlist.jsx
+++ Movie Manager/src/Watchlist/Watchlist.jsx
@@ -30,7 +30,7 @@ const Watchlist = () => {
       <div className=' bg-gray-800 text-gray-300 px-2 min-h-[90svh]'>
         <div className='max-w-[1200px] mx-auto pt-[60px] pb-[20px] sm:py-[30px] h-full grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-8'>
 
-          <div className={`col-span-5 ${watchlist.length <= 0? "block":"hidden"}`}>
+          <div className={`col-span-2 sm:col-span-3 md:col-span-4 lg:col-span-5 ${watchlist.length <= 0? "block":"hidden"}`}>
             <p className='font-Lato text-xl font-bold'>Your List is Empty Bro</p>
           </div>
 
